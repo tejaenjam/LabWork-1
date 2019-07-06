c40ff1f9f28aa1a50475eb47046cc3c1c3a124b4
satish kumbhar <satishkumbha9999@gmail.com>
2019-07-06 20:57:39 +0530

    update Lab 9 Ex 2

--- L9E2.java
+++ L9E2.java
@@ -1,20 +1,39 @@
-import java.util.Scanner;
-class L9E2{
-  public int calculateDifference(int n){
-    int sum = 0;
-    int sumOfSquares = 0;
-    for(int i = 1 ; i <= n;i++){
-      sumOfSquares = sumOfSquares + i*i;
-      sum = sum + i;
-    }
-    sum = sum * sum;
-    return sumOfSquares - sum;
-  }
-  public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter number");
-    int n = sc.nextInt();
-    L9E2 e = new L9E2();
-    System.out.println(e.calculateDifference(n));
-  }
+package com.capgemini.Lab9;
+
+import java.util.*;
+public class L9E2 {
+	public Map<Character,Integer> countCharacter(char[] ch) {
+		HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+		for(char c:ch) {
+			//System.out.println(c);
+			if(hm.containsKey(c)) {
+				int value = hm.get(c) + 1;
+				hm.put(c, value);
+			}
+			else {
+				hm.put(c, 1);
+			}
+		}
+		return hm;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of character u want in array");
+		int n = sc.nextInt();
+		char ch[] = new char[n];
+		sc.nextLine();
+		for(int i = 0;i < n;i++) {
+			System.out.println("Enter character");
+			String s = sc.nextLine();
+			char c = s.charAt(0);
+			ch[i] = c;
+		}
+		L9E2 e = new L9E2();
+		System.out.println(e.countCharacter(ch));
+		
+	}
+
 }
+
